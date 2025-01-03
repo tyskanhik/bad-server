85637217c90fdf49b7d44559dcb727ed062b3a20
Arctic Fox <tyskanhik@mail.ru>
2025-01-03 16:10:16 +0300

    add cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,13 +10,13 @@ import errorHandler from './middlewares/error-handler'
 import serveStatic from './middlewares/serverStatic'
 import routes from './routes'
 
-const { PORT = 3000 } = process.env
+const { PORT = 3000, ORIGIN_ALLOW = 'http://localhost:5173' } = process.env
 const app = express()
 
 app.use(cookieParser())
 
 app.use(cors())
-// app.use(cors({ origin: ORIGIN_ALLOW, credentials: true }));
+app.use(cors({ origin: ORIGIN_ALLOW, credentials: true }));
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(serveStatic(path.join(__dirname, 'public')))
