63ff773b59f070ec9fa2a92d07ef580e3ce0071a
Arctic Fox <tyskanhik@mail.ru>
2025-01-05 18:28:38 +0300

    sanitizedComment

--- backend/src/controllers/order.ts
+++ backend/src/controllers/order.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express'
 import { FilterQuery, Error as MongooseError, Types } from 'mongoose'
+import { escape } from "validator"
 import BadRequestError from '../errors/bad-request-error'
 import NotFoundError from '../errors/not-found-error'
 import Order, { IOrder } from '../models/order'
@@ -311,13 +312,15 @@ export const createOrder = async (
             return next(new BadRequestError('Неверная сумма заказа'))
         }
 
+        const sanitizedComment = escape(comment);
+
         const newOrder = new Order({
             totalAmount: total,
             products: items,
             payment,
             phone,
             email,
-            comment,
+            comment: sanitizedComment,
             customer: userId,
             deliveryAddress: address,
         })
